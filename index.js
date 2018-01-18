84fc46b454e87aadc65ac0a201458a93ae62dfd3
mistermappy123 <mikhal.gelo@gmail.com>
2018-01-17 23:09:21 -0500

    display country bio using 3digit code

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express(); 
+const fs = require('fs');
 
-app.get('/', function(req,res){
-	res.send(200);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'))
+
+app.get('/countries/:id', function(req,res){
+	fs.readFile('./countries.json', 'utf-8', function(err,data){
+		let countries = JSON.parse(data);
+		let country = req.params.id.toUpperCase();
+
+		//console.log(countries[country].population)
+		res.render('main', {countries: countries, country: country})
+	})
+	
+})
+
+app.get('/profile/:id', function(req,res){
+	res.send('You requested to see the profile of user: ' + req.params.id)
+
 })
 
 app.listen(3000, function(){
